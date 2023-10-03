ac5ce1f10fb92e08602137b31cb10de083d4a71a
Replit user <>
2023-10-03 01:11:31 +0000

    strings basic functions

--- String/MyString.java
+++ String/MyString.java
@@ -2,44 +2,93 @@ package String;
 
 public class MyString {
 
-    public static void main(String[] args){
-        String str =  "helloo";//literal
+    ublic static void main(String[] args) {
 
-        //print
-        System.out.println(str);
+		String str = "helloo"; // literal
 
-        //length
-        System.out.println(str.length());
+		// print
+		System.out.println(str);
 
-        //charAt
-        System.out.println(str.charAt(0));
-        char ch = str.charAt(2);
-        System.out.println(ch);
-        System.out.println(str.charAt(str.length()-1));
+		// length
+		System.out.println(str.length());
 
-        //substring
-        System.out.println(str.substring(1,3));//el
-        System.out.println(str.substring(1,5));//ello
-        System.out.println(str.substring(3,4));//l
-        System.out.println(str.substring(1,1));//""
-        System.out.println(str.substring(1));//ello
-        //System.out.println(str.substring(3,7)); //error
+		// charAt
+		System.out.println(str.charAt(0));
+		char ch = str.charAt(2); // l
+		System.out.println(ch);
+		System.out.println(str.charAt(str.length() - 1));
 
-        //indexof
-        System.out.println(str.indexOf("oo"));//4
-        System.out.println(str.indexOf("Ll")); //-1 : is not present
+		// substring
+		System.out.println(str.substring(1, 3)); // el
+		System.out.println(str.substring(1, 5)); // ello
+		System.out.println(str.substring(3, 4)); // l
+		System.out.println(str.substring(1, 1)); // ""
+		System.out.println(str.substring(1)); // ello
 
-        //lastindexof
-        System.out.println(str.lastIndexOf("o")); //5
+		// System.out.println(str.substring(3,7)); // error
 
-        //startsWith
-        System.out.println(Math.pow(10,2)*2);
+		// index of
+		System.out.println(str.indexOf("oo")); // 4
+		System.out.println(str.indexOf("Ll")); // -1 : is not present
 
+		// lastindexof
+		System.out.println(str.lastIndexOf("o")); // 5
 
+		// starts With
+		System.out.println(str.startsWith("He")); // false
+		System.out.println(str.startsWith("hel")); // true
 
+		// ends with
+		System.out.println(str.endsWith("lo")); // false
 
+		// concat
+		String s1 = "hi";
+		String s2 = "bye";
+		String s3 = s1 + s2;
+		System.out.println(s1 + ", " + s2 + ", " + s3);
+		String s4 = s1.concat(s2);
+		System.out.println(s1 + ", " + s2 + ", " + s4);
 
+		String s5 = "hhibyee";
+		String s6 = s5.substring(1, 6); // hibye
 
+		Scanner scn = new Scanner(System.in);
+		// String s7= scn.next() ; // hibye
 
-    }
+		// equals and ==
+		s1 = "Hello";
+		s2 = s1;
+		s3 = "Hello";
+		s4 = new String("Hello");
+
+		System.out.println((s1 == s2) + ", " + s1.equals(s2));
+		System.out.println((s1 == s3) + ", " + s1.equals(s3));
+		System.out.println((s1 == s4) + ", " + s1.equals(s4));
+
+		str.replace('l', 'q');
+		// System.out.println(str1);
+		System.out.println(str);
+
+		long start = System.currentTimeMillis();
+		appendString();
+		long end = System.currentTimeMillis();
+
+		System.out.println(end - start);
+
+	}
+
+	public static void appendString() {
+
+		int n = 100000;
+
+		// String str = "";
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= n; i++) {
+			// str += i;
+			sb.append(i);
+		}
+
+	}
+
 }
